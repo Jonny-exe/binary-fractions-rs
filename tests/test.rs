7b267d9eeba9ee795449461e1d4771893441c46a
alfred-pro <70751680+alfred-pro@users.noreply.github.com>
2022-10-16 16:20:38 +0200

    Update test.rs

--- tests/test.rs
+++ tests/test.rs
@@ -1,12 +1,26 @@
-#[path = "../src/binary.rs"]
-mod binary;
+// Integration tests
+extern crate binary_fractions;
+use binary_fractions::*;
+// use fraction::Fraction;
+use crate::binary::_BINARY_VERSION;
 
-// All the *integration* tests go here. 
-// The *unit* tests are inside the corresponding files.
-mod tests {
-    // Note this useful idiom: importing names from outer (for mod tests) scope.
-    #[test]
-    fn test_Binary() {
-        assert!(true);
-    }
+#[test]
+fn test_versions_const() {
+    let sub = &_BINARY_VERSION[..2];
+    assert_eq!(sub,"20");
 }
+
+
+// // This works but is not ideal.
+// #[path = "../src/binary.rs"]
+// mod binary;
+//
+// // All the *integration* tests go here.
+// // The *unit* tests are inside the corresponding files.
+// mod tests {
+//     // Note this useful idiom: importing names from outer (for mod tests) scope.
+//     #[test]
+//     fn test_Binary() {
+//         assert!(true);
+//     }
+// }
