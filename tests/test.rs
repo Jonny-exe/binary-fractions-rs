8b77a8df79877a6344dde842662a21e79e18c46a
alfred-pro <70751680+alfred-pro@users.noreply.github.com>
2022-10-16 22:11:46 +0200

    Update test.rs

--- tests/test.rs
+++ tests/test.rs
@@ -1,15 +1,27 @@
 // Integration tests
 extern crate binary_fractions;
 use binary_fractions::*;
+use crate::binary::*;
+use crate::twos_complement::*;
 // use fraction::Fraction;
-use crate::binary::_BINARY_VERSION;
 
 #[test]
-fn test_versions_const() {
+fn test_versions() {
     let sub = &_BINARY_VERSION[..2];
     assert_eq!(sub,"20");
 }
 
+#[test]
+fn test_binary_constructor() {
+    let b = Binary::from(5, None, None);
+    assert_eq!(b.to_string(),"");
+}
+
+#[test]
+fn test_twoscomplement_constructor() {
+    let b = TwosComplement::from(5);
+    assert_eq!(b.get(),5);
+}
 
 // // This works but is not ideal.
 // #[path = "../src/binary.rs"]
