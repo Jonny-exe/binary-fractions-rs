89449c6b11de70c4d319d902f6febf3e6321d010
alfred-pro <70751680+alfred-pro@users.noreply.github.com>
2022-10-16 16:21:26 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 //! # Floating-point Binary Fractions: Do math in base 2!
-//! 
-//! ```
+//!
+//!
+//! ```md
 //!  ████       ███
 //! ░░███      ░░░
 //!  ░███████  ████  ████████    ██████   ████████  █████ ████
@@ -12,7 +13,7 @@
 //!                                                  ███ ░███
 //!                                                 ░░██████
 //!                                                  ░░░░░░
-//! 
+//!
 //!     ██████                                 ███      ███
 //!    ███░░███                               ░███     ░░░
 //!   ░███ ░░░  ████████   ██████    ██████  ███████   ████   ██████  ████████    █████
@@ -22,11 +23,11 @@
 //!   █████     █████    ░░████████░░██████   ░░█████  █████░░██████  ████ █████ ██████
 //!  ░░░░░     ░░░░░      ░░░░░░░░  ░░░░░░     ░░░░░  ░░░░░  ░░░░░░  ░░░░ ░░░░░ ░░░░░░
 //! ```
-//! 
+//!
 //! This is an implementation of a floating-point binary fractions crate
 //! in Rust. This enables you to work with binary fractions and binary floats
 //! with ease!
-//! 
+//!
 //! This module allows one to represent integers, floats and fractions as
 //! binary strings.
 //! - e.g. the integer 3 will be represented as string '0b11'.
@@ -36,18 +37,18 @@
 //! '-0b0.01111e3', '-0b11.1e1' or '-0b1110e-2' all represent float -3.75.
 //! - two's complement representation is possible too:
 //! '11.11' for -1.25 in decimal, or '-0b1.01' in binary fraction.
-//! 
+//!
 //! Many operations and transformations are offered.
 //! You can sum, subtract, multiply, and divide long floating-point binary
 //! fractions. You can compute power of them, shift them left, shift them right,
 //! etc.
-//! 
+//!
 //! Basic representation of binary fractions and binary floats:
 //! A binary fraction is a subset of binary floats. Basically, a binary fraction
 //! is a binary float without an exponent (e.g. '-0b101.0101').
 //! Let's have a look at an example binary float value to see how it is represented.
-//! 
-//! ```
+//!
+//! ```md
 //!      prefix '0b' to indicate "binary" or "base 2"
 //!      ||
 //!      ||   decimal point
@@ -56,7 +57,7 @@
 //!      ||   |    |
 //!      ||   |    | exponent in base 10 (not in base 2!)
 //!      ||   |    | ||
-//!     -0b101.0101e-34  <-- example floating-point binary fraction
+//!    "-0b101.0101e-34"  <-- example floating-point binary fraction
 //!     |  ||| |||| |
 //!  sign  ||| |||| exponent sign
 //!        ||| ||||
@@ -64,20 +65,20 @@
 //!        |||
 //!        integer bits in base 2
 //! ```
-//! 
+//!
 //! If you are curious about floating point binary fractions, have a look at:
 //! - https://en.wikipedia.org/wiki/Computer_number_format#Representing_fractions_in_binary
 //! - https://www.electronics-tutorials.ws/binary/binary-fractions.html
 //! - https://ryanstutorials.net/binary-tutorial/binary-floating-point.php
 //! - https://planetcalc.com/862/
-//! 
+//!
 //! If you are curious about Two's complement:
 //! - https://janmr.com/blog/2010/07/bitwise-operators-and-negative-numbers/
 //! - https://en.wikipedia.org/wiki/Two%27s_complement
-//! 
+//!
 //! ## License:
 //! - GPL v3 or later
-//! 
+//!
 //! ## Features:
 //! - Rust
 //! - constructors for various types: int, float, Fraction, str, TwosComplement, Binary
@@ -96,32 +97,32 @@
 //!         ([binary-fractions](https://docs.rs/fraction/latest/binary-fractions/)).
 //! - well tested
 //!     - over 1600 test cases
-//! 
-//! 
+//!
+//!
 //! ## Sample usage, Example calls:
-//! 
+//!
 //! Please have a look at the short example program that uses the
 //! `Binary` crate. See `examples` directory in the repo.
-//! 
+//!
 //! ## Requirements:
 //! - Rust
 //! - Fraction crate
-//! 
+//!
 //! ## Installation:
 //! - see [https://crates.io/crates/binary-fractions/](https://crates.io/crates/binary-fractions/)
-//! 
+//!
 //! ## Testing, Maturity
 //! - run example(s) in `examples` directory to execute simple sample program(s)
 //! - run test cases in `tests` directory to execute all unit tests
 //! - `Binary` is relatively mature, more than 1600 test cases have been written and all
 //!     passed.
-//! 
+//!
 //! ## Contributions:
 //! - PRs are welcome and very much appreciated! :+1:
 //! - Please run and pass all existing 1600+ test cases before issuing a PR.
 //! - File Format: linted/beautified with `rustfmt`
-//! 
+//!
 //! Enjoy :heart: !
-//! 
+//!
 
-mod binary-fractions
+pub mod binary;
